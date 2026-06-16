1a23433baa0a357b6108c507e00d6f15762c4c58
Farid Eyvazov <ferideyvazov25@gmail.com>
2026-06-16 23:15:39 +0200

    fix: add flex centering to dialog close button icon

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -144,7 +144,7 @@ function DialogContent({
             type="button"
             data-slot="dialog-close"
             onClick={() => onOpenChange(false)}
-            className="ring-offset-background focus:ring-ring absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+            className="ring-offset-background focus:ring-ring absolute top-4 right-4 flex items-center justify-center rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
           >
             <XIcon />
             <span className="sr-only">Close</span>
